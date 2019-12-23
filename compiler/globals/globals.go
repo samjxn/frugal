b0476c6afa7be3822f9b982f8225a26af239d5f5
Brian Shannan <brian.shannan@workiva.com>
2019-12-23 10:13:39 -0600

    release 3.5.1

--- compiler/globals/globals.go
+++ compiler/globals/globals.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version of the Frugal compiler.
-const Version = "3.5.0"
+const Version = "3.5.1"
 
 // Global variables.
 var (
--- lib/python/frugal/version.py
+++ lib/python/frugal/version.py
@@ -10,4 +10,4 @@
 # limitations under the License.
 
 
-__version__ = '3.5.0'
+__version__ = '3.5.1'
