ee938b940659c869dd0100f24ca25c4d22e66668
josephswanson-wf <joseph.swanson@workiva.com>
2020-04-20 11:47:07 -0600

    INFENG-11402 remove codeov

--- scripts/smithy/smithy_python.sh
+++ scripts/smithy/smithy_python.sh
@@ -13,7 +13,6 @@ make xunit-py2
 # Write dependencies out so that RM is able to track them
 # The name of this file is hard coded into Rosie and RM console
 pip freeze > $FRUGAL_HOME/python2_pip_deps.txt
-$FRUGAL_HOME/scripts/smithy/codecov.sh $FRUGAL_HOME/lib/python/unit_tests_py2.xml python_two
 make flake8-py2
 deactivate
 
@@ -33,7 +32,5 @@ mv dist/frugal-*.tar.gz $FRUGAL_HOME
 # get coverage report in correct format
 coverage xml
 mv $FRUGAL_HOME/lib/python/coverage.xml $FRUGAL_HOME/lib/python/coverage_py3.xml
-
-$FRUGAL_HOME/scripts/smithy/codecov.sh $FRUGAL_HOME/lib/python/coverage_py3.xml python_three
 
 deactivate
